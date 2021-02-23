dbd3746dff30da802a0935423791f2997694be95
Shaked Shavit <69425857+ShakedShavit@users.noreply.github.com>
2021-02-23 20:24:30 +0200

    heroku deployment commit

--- server/app.js
+++ server/app.js
@@ -6,11 +6,9 @@ const userRouter = require('./routes/user');
 const bookRouter = require('./routes/book');
 
 const port = process.env.PORT || 5000;
-const publicDirectoryPath = path.join(__dirname, 'the path from the current directory');
 
 const app = express();
 
-app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 app.use(cors());
 
